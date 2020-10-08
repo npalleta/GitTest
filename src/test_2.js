696aa7ed7c90953c4a534e864c48e24708d40b80
npalleta <palleta.sp@gmail.com>
2020-10-07 22:42:35 -0300

    Teste C

--- src/test_2.js
+++ src/test_2.js
@@ -5,9 +5,12 @@ class Foo {
     methodA () {
         return 'Hi, dev';
     }
-
     
     methodB () {
         return 'Hi, js dev';
+    }
+
+    methodC () {
+        return 'Down in a hole';
     }
 }
